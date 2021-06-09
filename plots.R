e4494a9f83a6a27b1f6f02361818870c80f04e08
yusuf001 <yusuf001@cs.washington.edu>
2021-06-09 01:11:50 -0700

    Fixed bugs in the code that made the y-axis absurdly large. Still haven't done the analysis or summary yet.

--- plots.R
+++ plots.R
@@ -14,12 +14,15 @@ mwdata4 <- filter(mwdata3, State_Min_Wage > 0)
 mwdata5 <- select(mwdata4, State, Year, State_Min_Wage)
 View(mwdata4)
 
-mwdata6 <- select(mwdata4, Year, Federal_Min_Wage, Effective_Min_Wage)
+randommwdata <- mwdata %>% 
+  group_by(Year) %>% 
+  summarize(Year = Year, Federal_Min_Wage = mean(Federal.Minimum.Wage.2020.Dollars), Effective_Min_Wage = mean(Effective.Minimum.Wage.2020.Dollars))
+mwdata6 <- distinct(randommwdata)
 View(mwdata6)
 
 urdata2 <- urdata %>%
-  group_by(State, Year) %>%
-  summarize(Year = Year, State = State, Rate = mean(Rate))
+  group_by(Year) %>%
+  summarize(Year = Year, Rate = mean(Rate))
 urdata3 <- distinct(urdata2)
 View(urdata3)
 
