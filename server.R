492066dda2d3cdcdfb9c922951daf562b60607fa
yusuf001 <yusuf001@cs.washington.edu>
2021-06-09 00:04:57 -0700

    Updated project with 2 completed plots, but nothing else. No third plot, analysis, or conclusion yet.

---
+++ server.R
@@ -0,0 +1,46 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+library(ggplot2)
+library(maps)
+library(dplyr)
+
+mwdata <- read.csv("Minimum Wage Data.csv")
+urdata <- read.csv("output.csv")
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    output$distPlot <- renderPlot({
+        ##year <- input$year
+        yeardata <- filter(mwdata5, Year %in% input$year)
+        ggplot(data = yeardata, aes(x = State, y = State_Min_Wage)) +
+            geom_bar(stat = "identity", fill = "blue") +
+            theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+        
+    })
+    
+    output$Plot2 <- renderPlot({
+        if(input$radio == "1"){
+            ggplot(data = mwdata6, aes(x = Year, y = Federal_Min_Wage)) + geom_bar(stat = "identity", fill = "blue")
+        }else{
+            ggplot(data = mwdata6, aes(x = Year, y = Effective_Min_Wage)) + geom_bar(stat = "identity", fill = "green")
+        }
+    })
+    
+    output$Map <- renderPlot({
+        maps()
+    })
+    
+    output$summary <- renderText({
+        
+    })
+
+})
---
+++ ui.R
@@ -0,0 +1,77 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+mwdata <- read.csv("Minimum Wage Data.csv")
+urdata <- read.csv("output.csv")
+
+# Define UI for application that draws a histogram
+shinyUI(
+    navbarPage("INFO201 Final Project: US Minimum Wage and Unemployment Rate",
+               tabPanel("Unemployment Rate Correlation with Minimum Wage",
+                        sidebarLayout(
+                            sidebarPanel(
+                                sliderInput("bins",
+                                            "Number of bins:",
+                                            min = 1,
+                                            max = 50,
+                                            value = 30)
+                            ),
+                            
+                            # Show a plot of the generated distribution
+                            mainPanel(
+                                ##plotOutput("distPlot")
+                            ))),
+               tabPanel("State Minimum Wages",
+                        sidebarLayout(
+                            sidebarPanel(
+                                sliderInput("year",
+                                            "Year",
+                                            min = 1968,
+                                            max = 2020,
+                                            value = 1968)
+                            ),
+                            
+                            # Show a plot of the generated distribution
+                            mainPanel(
+                                plotOutput("distPlot")
+                            ))),
+               tabPanel("Minimum Wage Increase",
+                        sidebarLayout(
+                            sidebarPanel(
+                                radioButtons("radio",
+                                            "Federal vs Effective",
+                                            choices = list("Federal Minimum Wage" = 1, "Effective Minimum Wage" = 2), 
+                                            selected = 1)
+                            ),
+                
+                            
+                            # Show a plot of the generated distribution
+                            mainPanel(
+                               plotOutput("Plot2")
+                            ))),
+               tabPanel("Summary",
+                        sidebarLayout(
+                            sidebarPanel(
+                                sliderInput("bins",
+                                            "Number of bins:",
+                                            min = 1,
+                                            max = 50,
+                                            value = 30)
+                            ),
+                            
+                            
+                            # Show a plot of the generated distribution
+                            mainPanel(
+                               ## plotOutput("distPlot")
+                            )))
+               
+
+))
